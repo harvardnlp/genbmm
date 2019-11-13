5cc4f92208477a1b8b30686c3db4900f90b3693a
srush <srush.research@gmail.com>
2019-11-12 23:24:36 -0500

    Create matmul_cuda.cpp

---
+++ matmul_cuda.cpp
@@ -0,0 +1,50 @@
+#include <torch/extension.h>
+#include <vector>
+
+// CUDA forward declarations
+
+std::vector<torch::Tensor> matmul_cuda_forward(
+    torch::Tensor a,
+    torch::Tensor b);
+
+std::vector<torch::Tensor> matmul_cuda_backward(
+        torch::Tensor a,
+        torch::Tensor b,        
+        torch::Tensor grad_output,
+        torch::Tensor part
+        );
+
+// C++ interface
+
+// NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+std::vector<torch::Tensor> matmul_forward(
+    torch::Tensor a,
+    torch::Tensor b) {
+  CHECK_INPUT(a);
+  CHECK_INPUT(b);
+
+  return matmul_cuda_forward(a, b);
+}
+
+std::vector<torch::Tensor> matmul_backward(
+        torch::Tensor a,
+        torch::Tensor b,
+        torch::Tensor grad_output,
+        torch::Tensor part
+        ) {
+  CHECK_INPUT(a);
+  CHECK_INPUT(b);
+  CHECK_INPUT(grad_output);
+  CHECK_INPUT(part);
+  return matmul_cuda_backward(a, b, grad_output, part);
+}
+
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("forward", &matmul_forward, "Log-Matmul forward (CUDA)");
+  m.def("backward", &matmul_backward, "Log-Matmul backward (CUDA)");
+}
