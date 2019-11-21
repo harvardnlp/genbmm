cc808ee9d9f8de892015a0333209012f9afa689b
Sasha <arush@g.harvard.edu>
2019-11-21 09:54:21 -0500

    .

--- genbmm/__init__.py
+++ genbmm/__init__.py
@@ -1,4 +1,4 @@
-from .genmul import logbmm, samplebmm, maxbmm
+from .genmul import logbmm, samplebmm, maxbmm, bandedbmm, bandedlogbmm
 from .sparse import BandedMatrix, banddiag
 
-__all__ = [logbmm, samplebmm, maxbmm, BandedMatrix, banddiag]
+__all__ = [logbmm, samplebmm, maxbmm, BandedMatrix, banddiag, bandedbmm, bandedlogbmm]
