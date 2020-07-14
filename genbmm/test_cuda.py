64facc5a93a7350b51b0773a49da93d0e648cf0f
Alex Rush <sasha.rush@gmail.com>
2020-07-14 12:42:32 -0400

    backward

--- genbmm/test_cuda.py
+++ genbmm/test_cuda.py
@@ -11,10 +11,27 @@ sint = integers(min_value=3, max_value=5)
 
 @given(sint, mint, mint, mint)
 def test_logbmm(batch, row, inner, col):
-    a = torch.rand(batch, row, inner).cuda()
-    b = torch.rand(batch, inner, col).cuda()
+    a = torch.rand(batch, row, inner, requires_grad=True).cuda()
+    b = torch.rand(batch, inner, col, requires_grad=True).cuda()
 
     c = (a[:, :, :, None] + b[:, None, :, :]).logsumexp(-2)
     c2 = logbmm(a, b)
 
     assert(torch.isclose(c, c2).all())
+
+    g = torch.autograd.grad(c, (a, b), torch.rand(batch, row, col).cuda(), create_graph=True)
+    g2 = torch.autograd.grad(c, (a, b), torch.rand(batch, row, col).cuda(), create_graph=True)
+
+    for v1, v2 in zip(g, g2):
+        assert(torch.isclose(v1, v2).all())
+
+    
+    h = torch.autograd.grad((g[0], g[1]), (a, b), (torch.rand(batch, row, inner).cuda(),
+                                                   torch.rand(batch, inner, col).cuda()))
+    h2 = torch.autograd.grad((g2[0], g2[0]) , (a, b), (torch.rand(batch, row, inner).cuda(),
+                                                       torch.rand(batch, inner, col).cuda()))
+
+    for v1, v2 in zip(h, h2):
+        assert(torch.isclose(v1, v2).all())
+
+    
