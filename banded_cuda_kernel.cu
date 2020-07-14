7746d75a9f5e4a5f86b4bb10aded18f48960bf2e
Alex Rush <sasha.rush@gmail.com>
2020-07-14 19:07:00 -0400

    backward

--- banded_cuda_kernel.cu
+++ banded_cuda_kernel.cu
@@ -206,10 +206,10 @@ __global__ void banded_cuda_backbackward_kernel_A(
                 if (pos_in < 0 || pos_in >= n) continue;
 
                 scalar_t a_inner_val = a[batch][i][m];
-                scalar_t b_inner_val = b[batch][o][m2];
+                scalar_t b_inner_val = b[batch][pos_in][m2];
                 scalar_t s2 = exp(a_inner_val + b_inner_val - mx) / z;
                 scalar_t v;
-                if (j == m2) {
+                if (j == m) {
                     v = s  - s * s2;
                 } else {
                     v = - s * s2;
