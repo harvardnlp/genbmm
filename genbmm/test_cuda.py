b89800670e52f9373f909aeaed3f6b284d97b037
Alex Rush <sasha.rush@gmail.com>
2020-07-14 14:11:15 -0400

    backward

--- genbmm/test_cuda.py
+++ genbmm/test_cuda.py
@@ -52,17 +52,26 @@ def bmm_simple(a, b):
 
 @given(sint, mint, lint, lint)
 def test_sparse(batch, n, lu, ld):
-    start = torch.rand(batch, n, n)
+    start = torch.rand(batch, n, n, requires_grad=True)
     band, _ = banddiag(start, lu, ld)
     banded_x = BandedMatrix(band, lu, ld)
     banded_x_cuda = BandedMatrix(band.cuda(), lu, ld)
     x = banded_x.to_dense()
 
-    start = torch.rand(batch, n, n)
-    band, _ = banddiag(start, lu, ld)
+    start2 = torch.rand(batch, n, n, requires_grad=True)
+    band, _ = banddiag(start2, lu, ld)
     banded_y = BandedMatrix(band, lu, ld)
     banded_y_cuda = BandedMatrix(band.cuda(), lu, ld)
     y = banded_y.to_dense()
 
-    assert torch.isclose(bmm(banded_x_cuda, banded_y_cuda).data.cpu(),
-                         bmm_simple(banded_x, banded_y).data).all()
+
+    a = bmm(banded_x_cuda, banded_y_cuda).data
+    b = bmm_simple(banded_x, banded_y).data
+    assert torch.isclose(a.cpu(), b).all()
+
+    back = torch.rand(a.shape)
+    g = torch.autograd.grad(a, (start, start2), back.cuda(), create_graph=True)
+    g2 = torch.autograd.grad(b, (start, start2), back, create_graph=True)
+
+    for v1, v2 in zip(g, g2):
+        assert torch.isclose(v1.cpu(), v2).all()
