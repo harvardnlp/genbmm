671c7336d75851759845adb20772bc140a8b4b9d
Alex Rush <sasha.rush@gmail.com>
2020-07-16 17:42:17 -0400

    .

--- genbmm/test_cuda.py
+++ genbmm/test_cuda.py
@@ -135,7 +135,8 @@ def test_sparse(batch, n, lu, ld):
     # print(banded_x_cuda.transpose().data)
     print("START")
     a = BandedLogMul.apply(banded_y_cuda.data, lu, ld,
-                           repdiag(banded_x_cuda.data.flip(-1), lu, ld),
+                           banded_x_cuda.data,
+                           # repdiag(banded_x_cuda.data.flip(-1), lu, ld),
                            # Transpose.apply(banded_x_cuda.data, lu, ld),
                            ld, lu, ld+ld, lu+lu)
     # a = bmm(banded_x_cuda, banded_y_cuda).data
