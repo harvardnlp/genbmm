30575cc47b027246edddfd125e5bc001a9b84907
Alex Rush <sasha.rush@gmail.com>
2020-07-20 18:42:36 -0400

    .

--- genbmm/test_cuda.py
+++ genbmm/test_cuda.py
@@ -118,34 +118,21 @@ def test_sparse(batch, n, lu, ld):
     b = bmm_simple(banded_x, banded_y).data
     # b = banded_y.multiply_log_simple(banded_x.transpose()).data
     g2 = torch.autograd.grad(b, (start, start2), back, create_graph=True)
-    h2 = torch.autograd.grad((g2[0],), (start,), (back2[0],))
+    h2 = torch.autograd.grad((g2[0], g2[1]), (start, start2), (back2[0],))
 
 
     back = back.detach().clone().cuda()
     back.requires_grad_(True)
-
     start = band.detach().clone().cuda()
     start.requires_grad_(True)
     banded_x_cuda = BandedMatrix(start, lu, ld)
-
     start2 = band2.detach().clone().cuda()
     start2.requires_grad_(True)
     banded_y_cuda = BandedMatrix(start2, lu, ld)
-    # return b.multiply_log(a.transpose())
-    # print(banded_x_cuda.data)
-    # print(banded_x_cuda.transpose().data)
-    print("START")
-    # a = BandedLogMul.apply(banded_y_cuda.data, lu, ld,
-    #                        # banded_x_cuda.data,
-
-    #                        # repdiag(banded_x_cuda.data.flip(-1), lu, ld),
-    #                        Transpose.apply(banded_x_cuda.data, lu, ld),
-    #                        ld, lu, ld+ld, lu+lu)
     a = bmm(banded_x_cuda, banded_y_cuda).data
     g = torch.autograd.grad(a, (start, start2), back, create_graph=True)
-    print("MID")
     h = torch.autograd.grad((g[0], g[1]), (start, start2), (back2[0].cuda(), back2[1].cuda()))
-    print("END")
+
     for i, (v1, v2) in enumerate(zip(h, h2)):
         print(i)
         print(v1.shape)
