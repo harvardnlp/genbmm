079bc4da3e062c61a593ffe48723bf953c69a997
srush <srush.research@gmail.com>
2021-01-29 15:17:32 -0500

    both directions

--- opt/tvm_opt.py
+++ opt/tvm_opt.py
@@ -41,6 +41,7 @@ def logsummul(n, l, m, dtype):
         #lambda bb, ii, jj: te.sum(te.exp(A[bb, jj, k2] + B[bb, ii, k2]- M[bb, ii, jj]), axis=k2),
         name='M2')
 
+
     C = te.compute(
         (b, m, n),
         lambda bb, ii, jj: te.log(M2[bb, ii, jj]) + M[bb, ii, jj],
@@ -132,7 +133,117 @@ def logsummul(n, l, m, dtype):
     cache_read(M, AA, AL, BB, BL, ko, ki)
     cache_read(M2, AA2, AL2, BB2, BL2, ko2, ki2)
 
-    return s, [A, B, C]
+    return s, [A, B, C, M]
+
+
+@autotvm.template("logsummulback")
+def logsummulback(n, l, m, dtype):
+    nn = n
+    n = nn
+    bb = 1
+    b = bb
+    A = te.placeholder((bb, n, l), name='A', dtype=dtype)
+    B = te.placeholder((bb, m, l), name='B', dtype=dtype)
+    C = te.placeholder((b, m, n), name='C')
+    grad_C = te.placeholder((bb, m, n), name='grad_C', dtype=dtype)
+    M = te.placeholder((b, m, n), name='M')
+
+    k = te.reduce_axis((0, n), name='k')
+
+    grad_B = te.compute((bb, m, l),
+                            lambda bb, mm, ll:
+                            te.sum((te.exp(A[bb, k, ll] + B[bb, mm, ll] - M[bb, mm, k]) /
+                                    te.exp(C[bb, mm, k] - M[bb, mm, k])) * grad_C[bb, mm, k], axis=k),
+                            name='grad_B')
+    output = grad_B
+
+    s = te.create_schedule(output.op)
+
+    AA = s.cache_read(A, "shared", [output])
+    AL = s.cache_read(AA, "local", [output])
+    BB = s.cache_read(B, "shared", [output])
+    BL = s.cache_read(BB, "local", [output])
+    MM = s.cache_read(M, "shared", [output])
+    ML = s.cache_read(MM, "local", [output])
+    CC = s.cache_read(C, "shared", [output])
+    CL = s.cache_read(CC, "local", [output])
+    grad_CC = s.cache_read(grad_C, "shared", [output])
+    grad_CL = s.cache_read(grad_CC, "local", [output])
+
+
+    cfg = autotvm.get_config()
+    cfg.define_knob("y_bn", [32, 64, 128])
+    cfg.define_knob("x_bn", [32, 64, 128])
+    cfg.define_knob("y_t", [8, 32, 64])
+    cfg.define_knob("x_t", [2, 4, 8, 32])
+    cfg.define_knob("k_split", [1, 2, 8, 16])
+    unroll = True
+
+    b, y, x = s[output].op.axis
+    y_bn = cfg["y_bn"].val
+    x_bn = cfg["x_bn"].val
+    by, y = s[output].split(y, y_bn)
+    bx, x = s[output].split(x, x_bn)
+
+
+    y_nthreads = cfg["y_t"].val
+    x_nthreads = cfg["x_t"].val
+    ty, yi = s[output].split(y, nparts=y_nthreads)
+    tx, xi = s[output].split(x, nparts=x_nthreads)
+    thread_x = te.thread_axis((0, x_nthreads), "threadIdx.x")
+    thread_y = te.thread_axis((0, y_nthreads), "threadIdx.y")
+
+    s[output].reorder(b, by, bx, ty, tx, yi, xi)
+    s[output].bind(b, te.thread_axis("blockIdx.z"))
+    s[output].bind(by, te.thread_axis("blockIdx.y"))
+    s[output].bind(bx, te.thread_axis("blockIdx.x"))
+    s[output].bind(ty, thread_y)
+    s[output].bind(tx, thread_x)
+    if unroll:
+        s[output].pragma(yi, "auto_unroll_max_step", 16)
+
+    def cache_split(shared):
+        k, = s[shared].op.reduce_axis
+        ko, ki = s[shared].split(k, cfg["k_split"].val)
+        s[shared].reorder(ko, ki, yi, xi)
+        if unroll:
+            s[shared].pragma(ki, "auto_unroll_max_step", 16)
+
+        return ko, ki
+    ko, ki = cache_split(output)
+
+    def cache_read(shared, AA, AL, ko, ki):
+        s[AA].compute_at(s[shared], ko)
+        s[AL].compute_at(s[shared], ki)
+
+        _, y, k = s[AA].op.axis
+        ty, yi = s[AA].split(y, nparts=y_nthreads)
+        tx, ki = s[AA].split(k, nparts=x_nthreads)
+        s[AA].reorder(ty, tx, yi, ki)
+        s[AA].bind(ty, thread_y)
+        s[AA].bind(tx, thread_x)
+        if unroll:
+            s[AA].pragma(yi, "auto_unroll_max_step", 16)
+
+    cache_read(output, AA, AL, ko, ki)
+    cache_read(output, BB, BL, ko, ki)
+    cache_read(output, CC, CL, ko, ki)
+    cache_read(output, MM, ML, ko, ki)
+    cache_read(output, grad_CC, grad_CL, ko, ki)
+
+    return s, [A, B, C, M, grad_C, output]
+
+S1, S2, S3 = 512*512, 512, 256
+# print(tvm.lower(*logsummulback(S1, S2, S3, 'float32'), simple_mode=True))
+# exit()
+from tvm.contrib.dlpack import to_pytorch_func
+task = autotvm.task.create("logsummulback", args=(S1, S2, S3, 'float32',), target='cuda', target_host="llvm")
+with autotvm.apply_history_best('matmul.log'):
+    with tvm.target.Target("cuda"):
+        s_mult, arg_bufs = logsummulback(S1, S2, S3, 'float32')
+        mod = tvm.build(s_mult, arg_bufs)
+logsumback_pytorch = to_pytorch_func(mod)
+
 
 def get_abc(shape, constructor=None):
         """Return random a, b and empty c with the same shape.
@@ -145,15 +256,14 @@ def get_abc(shape, constructor=None):
             a, b, c = [constructor(x) for x in (a, b, c)]
         return a, b, c
 
-task = autotvm.task.create("logsummul", args=(512, 256, 512*512, 'float32',), target='cuda', target_host="llvm")
+task = autotvm.task.create("logsummul", args=(S1, S2, S3, 'float32',), target='cuda', target_host="llvm")
 with autotvm.apply_history_best('matmul.log'):
     with tvm.target.Target("cuda"):
-        s_mult, arg_bufs = logsummul(512, 256, 512*512, 'float32')
+        s_mult, arg_bufs = logsummul(S1, S2, S3, 'float32')
         mod = tvm.build(s_mult, arg_bufs)
 
-from tvm.contrib.dlpack import to_pytorch_func
-
 logsum_pytorch = to_pytorch_func(mod)
+
 
 #import torch
 #out = torch.rand(1, 512*512, 512).cuda()
@@ -166,7 +276,7 @@ if __name__ == "__main__":
 
     from tvm import autotvm
 
-    task = autotvm.task.create("logsummul", args=(512, 256, 512*512,'float32',), target='cuda', target_host="llvm")
+    task = autotvm.task.create("logsummul", args=(S1, S2, S3,'float32',), target='cuda', target_host="llvm")
     print(task.config_space)
 
     logging.getLogger("autotvm").setLevel(logging.DEBUG)
@@ -181,16 +291,34 @@ if __name__ == "__main__":
                measure_option=measure_option,
                callbacks=[autotvm.callback.log_to_file('matmul.log')])
 
-    def get_abc(shape, constructor=None):
-        """Return random a, b and empty c with the same shape.
-        """
-        np.random.seed(0)
-        a = np.random.normal(size=shape).astype(np.float32)
-        b = np.random.normal(size=shape).astype(np.float32)
-        c = np.empty_like(a)
-        if constructor:
-            a, b, c = [constructor(x) for x in (a, b, c)]
-        return a, b, c
+    task = autotvm.task.create("logsummulback", args=(S1, S2, S3,'float32',), target='cuda', target_host="llvm")
+    print(task.config_space)
+
+    measure_option = autotvm.measure_option(
+        builder=autotvm.LocalBuilder(n_parallel=5),
+        runner=autotvm.LocalRunner(number=5, repeat=2, timeout=60, min_repeat_ms=50))
+
+
+    tuner = autotvm.tuner.RandomTuner(task)
+    tuner.tune(n_trial=10,
+               measure_option=measure_option,
+               callbacks=[autotvm.callback.log_to_file('matmul.log')])
+
+
+
+
+
+
+    # def get_abc(shape, constructor=None):
+    #     """Return random a, b and empty c with the same shape.
+    #     """
+    #     np.random.seed(0)
+    #     a = np.random.normal(size=shape).astype(np.float32)
+    #     b = np.random.normal(size=shape).astype(np.float32)
+    #     c = np.empty_like(a)
+    #     if constructor:
+    #         a, b, c = [constructor(x) for x in (a, b, c)]
+    #     return a, b, c
 
     # autotvm.record.pick_best("matmul.log", "best.log")
     # with autotvm.apply_history_best('best.log'):
